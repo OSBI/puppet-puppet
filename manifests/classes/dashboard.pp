e2db495e7cc465914e6e8cda6d9e34b09c62b44d
Tom Barber <tom@analytical-labs.com>
2011-10-18 07:33:57 +0100

    dashboard nonsense

--- manifests/classes/dashboard.pp
+++ manifests/classes/dashboard.pp
@@ -4,7 +4,7 @@ package { "puppet-dashboard":
 	ensure => present,	
 }
 
-file { "/etc/default/puppet-dashboard"
+file { "/etc/default/puppet-dashboard":
 	ensure => present,
 	content => template('puppet/puppet-dashboard_default.erb'),
 	require => Package["puppet-dashboard"],
