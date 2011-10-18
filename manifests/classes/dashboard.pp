1dd60c43ea63c05d2c206ff208e6f9111a2aeb03
Tom Barber <tom@analytical-labs.com>
2011-10-18 07:47:36 +0100

    dashboard nonsense

--- manifests/classes/dashboard.pp
+++ manifests/classes/dashboard.pp
@@ -1,5 +1,25 @@
 class puppet::dashboard {
 
+include mysql::server
+
+    $puppet_mysql_username      = extlookup('puppet_mysql_username'      ,'')
+	$puppet_mysql_password      = extlookup('puppet_mysql_password'      ,'')
+
+mysql::database{"dashboard_production":
+  ensure   => present,
+  require => Class["mysql::server"]
+}
+
+
+mysql::rights{"dashboard database rights":
+  ensure   => present,
+  database => "dashboard_production",
+  user     => "${puppet_mysql_username}",
+  password => "${puppet_mysql_password}",
+  require => Class["mysql::server"],
+}
+
+
 package { "puppet-dashboard":
 	ensure => present,	
 }
@@ -10,9 +30,17 @@ file { "/etc/default/puppet-dashboard":
 	require => Package["puppet-dashboard"],
 }
 
+file { "/usr/share/puppet-dashboard/config/database.yml":
+	ensure => present,
+	content => template('puppet/database.yml.erb'),
+	require => Package["puppet-dashboard"],
+}
+
 service { "puppet-dashboard":
   ensure => "running",
   require => File["/etc/default/puppet-dashboard"],
 }
+
+
 
 }
